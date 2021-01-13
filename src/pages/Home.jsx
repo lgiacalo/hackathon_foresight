fc9c25ad8c2a06dd4674e262e2a911cb15b97c59
Valerie TV <vtylskivincent@gmail.com>
2021-01-13 22:49:27 +0100

    finish home page responsive layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,9 @@ import "../css/home.css";
 const Home = () => {
   return (
     <div className="pageContainer Home">
-      <h2>Home Page</h2>
       <Link to="/categories">Liste Catégories</Link> |
       <Link to="/toilettes-occidentaux">Catégorie Toilette sèche</Link> |
       <Link to="/toilettes-occidentaux/produit1">Toilette sèche produit 1</Link>{" "}
-      |
       <section>
         <article className="animation-phare">
           <Link to="/" title="">
@@ -40,13 +38,72 @@ const Home = () => {
           </article>
         </div>
       </section>
-      <article className="productCard">
-        <img
-          className="productImg"
-          src="https://francoischarron.com/datascontent/toilette-numi-lumieres.jpg"
-          alt=""
-        />
-        <p>Titre produit</p>
+      <article className="bests">
+        <div className="best-product-list">
+          <h2>Notre sélection de toilettes high-tech</h2>
+          <article className="best-product-card">
+            <Link to="/" title="Plus d'info">
+              <img
+                className="productImg"
+                src="https://francoischarron.com/datascontent/toilette-numi-lumieres.jpg"
+                alt=""
+              />
+              <h3>Luxe Platinium 2 Plus</h3>
+              <span className="best-product-link">Plus d'info</span>
+            </Link>
+          </article>
+          <article className="best-product-card">
+            <Link to="/" title="Plus d'info">
+              <img
+                className="productImg"
+                src="https://francoischarron.com/datascontent/toilette-numi-lumieres.jpg"
+                alt=""
+              />
+              <h3>Dual Flush Elongated Toilet</h3>
+              <span className="best-product-link">Plus d'info</span>
+            </Link>
+          </article>
+          <Link to="/" title="Tous les toilettes japonais">
+            Voir tous les toilettes japonais
+          </Link>
+        </div>
+        <div className="best-product-list">
+          <h2>Notre sélection de toilettes classiques</h2>
+          <article className="best-product-card">
+            <Link to="/" title="Plus d'info">
+              <img
+                className="productImg"
+                src="https://francoischarron.com/datascontent/toilette-numi-lumieres.jpg"
+                alt=""
+              />
+              <h3>Presqu'île</h3>
+              <span className="best-product-link">Plus d'info</span>
+            </Link>
+          </article>
+          <article className="best-product-card">
+            <Link to="/" title="Plus d'info">
+              <img
+                className="productImg"
+                src="https://francoischarron.com/datascontent/toilette-numi-lumieres.jpg"
+                alt=""
+              />
+              <h3>Escale</h3>
+              <span className="best-product-link">Plus d'info</span>
+            </Link>
+          </article>
+          <Link to="/" title="Tous les toilettes japonais">
+            Voir tous les toilettes occidentaux
+          </Link>
+        </div>
+      </article>
+      <article className="chapo">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
       </article>
     </div>
   );
