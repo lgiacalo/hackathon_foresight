4425b61c9b50f36fc9e72c1a12c349c2df7325d5
e-floran <e-floran@hotmail.fr>
2021-01-13 23:17:43 +0100

    styling categories, part 1

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -3,10 +3,11 @@ import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import CategorieCard from "../components/CategorieCard";
 import data from "../assets/toilets.json";
+import "../css/categories.css";
 
 const Categories = () => {
   return (
-    <div className="pageContainer">
+    <div className="pageContainer categoryContainer">
       <Helmet>
         <title>Cabinett - Categories</title>
         {/* <link rel="canonical" href="" /> */}
@@ -20,9 +21,11 @@ const Categories = () => {
       </nav>
 
       <h2>Liste des Categories</h2>
+      <section className="multipleCategoriesContainer">
       {data.categories.map((c) => (
         <CategorieCard key={c.slug_category} {...c} />
       ))}
+      </section>
     </div>
   );
 };
