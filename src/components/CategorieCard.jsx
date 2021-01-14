a4dc09cb31f2b38f454d5427a18d64e017ccde57
lgiacalo <lgiacalo@student.42.fr>
2021-01-14 09:48:41 +0100

    add slash

--- src/components/CategorieCard.jsx
+++ src/components/CategorieCard.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 const CategorieCard = ({ name, description, image, slug_category }) => {
   return (
     <article>
-      <h3>
-        <Link to={`/${slug_category}`}>{name}</Link>
-      </h3>
+      <h2>
+        <Link to={`/${slug_category}/`}>{name}</Link>
+      </h2>
       <p>{description}</p>
       <p>{image}</p>
     </article>
