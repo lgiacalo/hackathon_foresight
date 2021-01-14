22d78f0ed5fbde5ca18205310852c5498c64e8c0
lgiacalo <lgiacalo@student.42.fr>
2021-01-14 18:09:29 +0100

    edit title guideDetails

--- src/pages/GuideDetails.jsx
+++ src/pages/GuideDetails.jsx
@@ -28,9 +28,7 @@ const GuideDetails = () => {
           name="twitter:url"
           content={`https://cabinett.netlify.app/guides/${slug_guide}/`}
         />
-        <title>
-          Cabinett - Votre expert en toilettes - Guide - {guide.name}
-        </title>
+        <title>Cabinett - Votre expert en toilettes - {guide.name}</title>
         <link
           rel="canonical"
           href={`https://cabinett.netlify.app/guides/${slug_guide}/`}
