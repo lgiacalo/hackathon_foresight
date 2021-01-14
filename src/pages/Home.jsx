8963f352c7a9297facbc18f5dde4875e6068a40f
Valerie TV <vtylskivincent@gmail.com>
2021-01-14 17:08:03 +0100

    added text on hp

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,8 +16,7 @@ const Home = () => {
           content="Le spécialiste des toilettes, en tout genre, de tous les pays et pour tous les prix !"
         />
       </Helmet>
-      <h1>Home Page</h1>
-      <Link to="/categories/">Liste Catégories</Link>
+      <h1>Cabi.net, votre expert en toilettes</h1>
       <section>
         <article className="animation-phare">
           <Link
@@ -40,14 +39,11 @@ const Home = () => {
             </Link>
           </article>
           <article className="animation-dual">
-            <Link
-              to="/guides/comment-installer-ses-toilettes/"
-              title="Comment installer ses toilettes ?"
-            >
+            <Link to="/categories/" title="Toutes nos catégories de WC">
               <img
                 className="animation-triplet"
-                src="https://via.placeholder.com/800x342.png?text=Comment+installer+ses+toilettes+?"
-                alt="Comment installer ses toilettes ?"
+                src="https://via.placeholder.com/800x342.png?text=Toutes-nos-categories-de-WC"
+                alt="Voir toutes nos catégories de WC"
               />
             </Link>
           </article>
@@ -57,57 +53,72 @@ const Home = () => {
         <div className="best-product-list">
           <h2>Notre sélection de toilettes high-tech</h2>
           <article className="best-product-card">
-            <Link to="/" title="Plus d'info">
+            <Link to="/toilettes-japonaises/k-3900-kohler/" title="Plus d'info">
               <img
                 className="productImg"
-                src="https://francoischarron.com/datascontent/toilette-numi-lumieres.jpg"
-                alt=""
+                src="/products/toilettes-k-3900-kohler.png"
+                alt="Modèle K-3900 Numi One Piece Elongated Toilet"
               />
-              <h3>Luxe Platinium 2 Plus</h3>
+              <h3>Modèle K-3900 Numi One Piece Elongated Toilet</h3>
               <span className="best-product-link">Plus d'info</span>
             </Link>
           </article>
           <article className="best-product-card">
-            <Link to="/" title="Plus d'info">
+            <Link
+              to="/toilettes-japonaises/luxe-diamond-toptoilet/"
+              title="Plus d'info"
+            >
               <img
                 className="productImg"
-                src="https://francoischarron.com/datascontent/toilette-numi-lumieres.jpg"
-                alt=""
+                src="/products/toilettes-luxe-diamond-toptoilet.png"
+                alt="Modèle Luxe Diamond Plus"
               />
-              <h3>Dual Flush Elongated Toilet</h3>
+              <h3>Modèle Luxe Diamond Plus</h3>
               <span className="best-product-link">Plus d'info</span>
             </Link>
           </article>
-          <Link to="/" title="Tous les toilettes japonais">
-            Voir tous les toilettes japonais
+          <Link
+            to="/toilettes-japonaises/"
+            title="Toutes les toilettes japonaises"
+          >
+            Voir toutes les toilettes japonaises
           </Link>
         </div>
         <div className="best-product-list">
           <h2>Notre sélection de toilettes classiques</h2>
           <article className="best-product-card">
-            <Link to="/" title="Plus d'info">
+            <Link
+              to="/toilettes-occidentales/cavally-goodhome/"
+              title="Plus d'info"
+            >
               <img
                 className="productImg"
-                src="https://francoischarron.com/datascontent/toilette-numi-lumieres.jpg"
-                alt=""
+                src="/products/toilettes-cavally-goodhome.jpg"
+                alt="Modèle Cavally"
               />
-              <h3>Presqu'île</h3>
+              <h3>Modèle Cavally</h3>
               <span className="best-product-link">Plus d'info</span>
             </Link>
           </article>
           <article className="best-product-card">
-            <Link to="/" title="Plus d'info">
+            <Link
+              to="/toilettes-occidentales/isis-swarovski/"
+              title="Plus d'info"
+            >
               <img
                 className="productImg"
-                src="https://francoischarron.com/datascontent/toilette-numi-lumieres.jpg"
-                alt=""
+                src="/products/toilettes-isis-swarovski.jpg"
+                alt="Modèle Isis"
               />
-              <h3>Escale</h3>
+              <h3>Modèle Isis</h3>
               <span className="best-product-link">Plus d'info</span>
             </Link>
           </article>
-          <Link to="/" title="Tous les toilettes japonais">
-            Voir tous les toilettes occidentaux
+          <Link
+            to="/toilettes-occidentales/"
+            title="Toutes les toilettes occidentales"
+          >
+            Voir toutes les toilettes occidentales
           </Link>
         </div>
       </article>
@@ -116,9 +127,18 @@ const Home = () => {
         représente en moyenne 3 ans de notre vie&nbsp;! Chez{" "}
         <strong>Cabi.net</strong>, nous avons décidé de vous faciliter la tâche
         pour trouver le WC qui vous convienne. Envie de goûter au confort des{" "}
-        <Link to="/toilettes-occidentales/">
+        <Link to="/toilettes-japonaises/">
           toilettes de luxe à la japonaise
         </Link>{" "}
+        sans avoir à prendre l'avion ? Visitez notre catégorie spéciale pour
+        vous équiper ! Sur Cabi.net, vous trouverez des WC tous plus originaux
+        les uns que les autres, correspondant à tous les besoins et à tous les
+        budgets... Des{" "}
+        <Link to="/toilettes-classiques/">toilettes économiques</Link> au
+        <Link to="/toilettes-japonaises/"> toilettes Grand Luxe</Link>, sans
+        oublier les <Link to="/toilettes-seches/">toilettes écologiques</Link>,
+        avec Cabi.net, faites de votre petit coin le lieu incontournable de chez
+        vous !
       </article>
     </main>
   );
